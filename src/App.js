0b9c14d4c7f47efb8c8e28d0282e5d547023a9c1
Thang Vu <thangvu.dona@gmail.com>
2023-11-01 09:17:02 +0700

    Routing - ReactJS
    - BrowserRouter > Routes > Route (path. component, ...)
    - Link, NavLink
    - Switch > Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 // import './App.css';
-import ColorBox from './components/ColorBox';
+import { Link, NavLink, Route, Routes } from 'react-router-dom';
 import TodoFeature from './features/Todo';
+import AlbumFeature from './features/AlbumSong';
 
 function App() {
   return (
     <div className="App">
-      <TodoFeature />
-      {/* <ColorBox /> */}
+      Header
+      <p><Link to='/todos'>Todos</Link></p>
+      <p><Link to='/albums'>Albums</Link></p>
+
+      <p><NavLink to='/todos' activeClassName="active-menu">Todos</NavLink></p>
+      <p><NavLink to='/albums'>Albums</NavLink></p>
+
+      <Routes>
+        <Route path='/todos' Component={TodoFeature} />
+        <Route path='/albums' Component={AlbumFeature} />
+      </Routes>
+      Footer
     </div>
   );
 }
