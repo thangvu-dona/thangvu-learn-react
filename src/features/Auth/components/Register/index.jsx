faa26752966dc4234c7462ac07b2db61899c5284
Thang Vu <thangvu.dona@gmail.com>
2023-11-06 11:48:38 +0700

    Use asynce action on  component
    - dispath action
    - unwrapResult

--- src/features/Auth/components/Register/index.jsx
+++ src/features/Auth/components/Register/index.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 import RegisterForm from "../RegisterForm";
+import { useDispatch } from "react-redux";
+import { register } from "../../userSlice";
+import { unwrapResult } from "@reduxjs/toolkit";
 
 Register.propTypes = {};
 
 function Register(props) {
-  const handleFormSubmit = (value) => {
-    console.log("Register form submit: ", value);
+  const dispatch = useDispatch();
+
+  const handleFormSubmit = async (values) => {
+    // console.log("Register form submit: ", values);
+
+    try {
+      // auto set username = email
+      values.username = values.email;
+
+      const action = register(values);
+      const resultAction = await dispatch(action);
+      const user = unwrapResult(resultAction);
+
+      // do something here on register successfully
+
+      console.log("New user: ", user);
+    } catch (error) {
+      console.log("Fail to register!", error);
+    }
   };
 
   return (
--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -2,14 +2,14 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import userApi from "../../api/userApi";
 
 // First, create the thunk
-const register = createAsyncThunk(
+export const register = createAsyncThunk(
   'users/register',
   async (payload) => {
     // call API to register
     const data = await userApi.register(payload)
     // save data to local storage
     localStorage.setItem('access_token', data.jwt);
-    localStorage.setItem('usre', JSON.stringify(data.user));
+    localStorage.setItem('user', JSON.stringify(data.user));
 
     // return user data
     return data.user;
