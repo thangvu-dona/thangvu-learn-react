45d9753758d5d1cd4bd097256e03640417a6de04
Thang Vu <thangvu.dona@gmail.com>
2023-11-06 10:57:31 +0700

    Redux: create User
    - create Reducer - Action --> use createSlice.
    - add reducer to store.
    - create brand new userApi.

---
+++ src/features/Auth/userSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    current: {},
+    settings: {},
+  },
+  reducers: {
+
+  }
+});
+
+const { reducer } = userSlice;
+export default reducer;
