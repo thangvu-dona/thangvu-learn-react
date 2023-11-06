1f00a921491cd030f70c6b953a6e17602f2b67db
Thang Vu <thangvu.dona@gmail.com>
2023-11-06 11:22:41 +0700

    Create Async Action: register
    - use createAsynceThunk (ReduxThunk middleware) to create async action.
    - handle fulfilled: update state get from action payload

--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -1,4 +1,20 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import userApi from "../../api/userApi";
+
+// First, create the thunk
+const register = createAsyncThunk(
+  'users/register',
+  async (payload) => {
+    // call API to register
+    const data = await userApi.register(payload)
+    // save data to local storage
+    localStorage.setItem('access_token', data.jwt);
+    localStorage.setItem('usre', JSON.stringify(data.user));
+
+    // return user data
+    return data.user;
+  }
+)
 
 const userSlice = createSlice({
   name: 'user',
@@ -6,9 +22,13 @@ const userSlice = createSlice({
     current: {},
     settings: {},
   },
-  reducers: {
-
-  }
+  reducers: {},
+  extraReducers: {
+    // register.fulfilled is string like: 'users/register/fulfilled'
+    [register.fulfilled]: (state, action) => {
+      state.current = action.payload;
+    }
+  },
 });
 
 const { reducer } = userSlice;
