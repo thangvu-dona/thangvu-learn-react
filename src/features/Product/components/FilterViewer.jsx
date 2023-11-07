c23bb1b14cf3dadd77ed71e9204474e69684ac5e
Thang Vu <thangvu.dona@gmail.com>
2023-11-08 03:48:57 +0700

    useMemo for prop - filters change

--- src/features/Product/components/FilterViewer.jsx
+++ src/features/Product/components/FilterViewer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import PropTypes from "prop-types";
 import { Box, Chip } from "@mui/material";
 import { makeStyles } from "@mui/styles";
@@ -88,9 +88,13 @@ const FILTER_LIST = [
 function FilterViewer({ filters = {}, onChange = null }) {
   const classes = useStyles();
 
+  const visibleFilters = useMemo(() => {
+    return FILTER_LIST.filter((x) => x.isVisible(filters));
+  }, [filters]);
+
   return (
     <Box component="ul" className={classes.root}>
-      {FILTER_LIST.filter((x) => x.isVisible(filters)).map((x) => (
+      {visibleFilters.map((x) => (
         <li key={x.id}>
           <Chip
             label={x.getLabel(filters)}
